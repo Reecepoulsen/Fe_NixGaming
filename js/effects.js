9a476182f7c06eab94dbbc0db492beefdcc4ba4a
Reece Poulsen <dreecepoulsen@gmail.com>
2022-03-07 16:12:07 -0700

    first commit

---
+++ js/effects.js
@@ -0,0 +1,29 @@
+// Navbar fade effect
+const navbar = document.getElementById("home-navbar")
+const nav_transition_point = navbar.offsetTop
+
+const transitionNav = () => {
+  if (window.scrollY > nav_transition_point) {
+    navbar.classList.add("navbar-transition")
+  } else {
+    navbar.classList.remove("navbar-transition")
+  }
+}
+
+
+// Slide in effect
+const missionText = document.getElementById("mission-text");
+const mission_transition_point = missionText.offsetTop;
+
+const transitionMission = () => {
+  if (window.scrollY > mission_transition_point) {
+    navbar.classList.add("animate-mission")
+  } else {
+    navbar.classList.remove("animate-mission")
+  }
+}
+
+window.onscroll = () => {
+  transitionNav();
+  transitionMission();
+}
